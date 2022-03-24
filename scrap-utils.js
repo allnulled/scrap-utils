3670bf88af72f69fdf699f2adc8c38c6383f3c97
allnulled <todosanulados@gmail.com>
2022-03-24 16:11:06 +0100

    added user prompt

--- scrap-utils.js
+++ scrap-utils.js
@@ -37,6 +37,7 @@ window.triggerMouseEvent = function (nodeP, eventType) {
     node.dispatchEvent(clickEvent);
 };
 /**
+ * 
  * @description Performs a programmatic click event against HTML elements.
  * @param {String|HTMLElement} targetNode - CSS Selector or HTML Element to click.
  * @returns {Undefined}
@@ -46,4 +47,65 @@ window.triggerClick = function (targetNode) {
     triggerMouseEvent(targetNode, "mousedown");
     triggerMouseEvent(targetNode, "mouseup");
     triggerMouseEvent(targetNode, "click");
+};
+/**
+ * 
+ * @param {String} mensaje - Required. Message of the question.
+ * @param {String} defecto - Default answer.
+ * @param {Function} hastaQue - Answer validator function. Must return a boolean.
+ * @param {String|undefined} mensajeHastaQue - Invalid answer message.
+ * @param {Function} modifier - Modifier function of the valid answer.
+ * @returns {Promise <String>} answer - Text of the answer of the user.
+ */
+const ask_until_original = function(mensaje, defecto = "", hastaQue = () => true, mensajeHastaQueP = undefined, modifier = o => o) {
+    const mensajeHastaQue = mensajeHastaQueP || "Error: la respuesta introducida no es válida.";
+    if(typeof window === "object") {
+        return new Promise(ok => {
+            let isOk = false;
+            while(!isOk) {
+                const respuesta = window.prompt(mensaje, defecto);
+                isOk = hastaQue(respuesta);
+                if(isOk) {
+                    return modifier(respuesta);
+                }
+                window.alert(mensajeHastaQue);
+            }
+        });
+    } else if(typeof global === "object") {
+        const readline = require("readline");
+        const reader = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+        return new Promise(ok => {
+            let ask = () => {
+                reader.question(mensaje + "\n«Respuesta:» ", respuestaP => {
+                    const respuesta = respuestaP === "" ? defecto : respuestaP;
+                    const isOk = hastaQue(respuesta);
+                    if(!isOk) {
+                        console.error(mensajeHastaQue);
+                        console.error("");
+                        return ask();
+                    }
+                    reader.close();
+                    return ok(modifier(respuesta));
+                });
+            };
+            ask();
+        });
+    }
+};
+/**
+ * 
+ * @param {Object} opt - Can have: { message:String, default:String, validate:Function, error:String, modifier:Function }.
+ * @returns {Promise <String>} answer - The answer of the user.
+ */
+window.ask_until = function(opt) {
+    return ask_until_original(
+        opt.message,
+        opt.default,
+        opt.validate || () => true,
+        opt.error || undefined,
+        opt.modifier || o => o
+    );
 };
